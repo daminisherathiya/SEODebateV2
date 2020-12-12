63d0859269f1bdeb66cda03bb966c7274a3270be
damini sherathiya <daminisherathiya@gmail.com>
2020-12-12 16:28:13 +0530

    Fixes.

--- js/SEODebate.js
+++ js/SEODebate.js
@@ -4,8 +4,18 @@ const PAGINATION_ITEMS_PER_PAGE = document.querySelectorAll("#id-pagination-data
 $(function () {
   // Latest section
   pagination();
+  video_btn();
 });
 
+function video_btn() {
+  document.querySelector(".btn-setter").addEventListener("click",function(){
+    document.querySelector(".video-container .btn-setter").remove();
+    document.querySelector(".video-container img").remove();
+    document.querySelector(".embed-responsive").classList.remove("d-none");
+    document.querySelector("iframe").setAttribute("src", document.querySelector("iframe").getAttribute("src") + "?autoplay=1");
+    // $("iframe")[0].src += "?autoplay=1";
+  })
+}
 
 function pagination() {
   populate_data_for_pagination();
